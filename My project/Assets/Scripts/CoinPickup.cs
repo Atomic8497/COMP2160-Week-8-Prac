8bfa05b54fbafb13bfb4bb3f275051945e481998
lachlanmurray <lachlanmurray02@gmail.com>
2023-10-17 21:53:49 +1100

    Added player and coin scripts, ready for merge and step 3

---
+++ My project/Assets/Scripts/CoinPickup.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinPickup : MonoBehaviour
+{
+
+    private GameManager gameManager; // Game Manager
+
+    void Awake()
+    {
+        gameManager = (GameManager) FindObjectOfType(typeof(GameManager)); // set game manager
+        if(!gameManager)
+        {
+            Debug.LogError("There is no game manager in the scene");
+        }
+    }
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("Hits");
+        if(collision.gameObject.tag == "Player"){
+            gameManager.Score();
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ My project/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    [SerializeField] private int moveSpeed = 1;
+    private PlayerActions movement;
+    private InputAction direction;
+    private Vector3 position;
+    void Awake()
+    {
+        movement = new PlayerActions();
+        direction = movement.PlayerMovement.Direction;
+    }
+
+    void Start()
+    {
+        
+    }
+
+    void OnEnable()
+    {
+        direction.Enable();
+    }
+
+    void OnDisable()
+    {
+        direction.Disable();
+    }
+
+    void Update()
+    {
+        position = direction.ReadValue<Vector2>();
+        position.z = position.y;
+        position.y = 0;
+        transform.Translate(position * Time.deltaTime * moveSpeed, Space.Self);
+    }
+}
